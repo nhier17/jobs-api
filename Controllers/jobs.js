d4a5fc849866abca108b341be05f9ddf05f12c1d
Abraham <abrahamnhier97@gmail.com>
2024-01-25 19:02:41 +0300

    finished setting up jobs controllers functionality

--- Controllers/jobs.js
+++ Controllers/jobs.js
@@ -3,11 +3,20 @@ const {StatusCodes} = require('http-status-codes')
 const {BadRequestError, NotFoundError} = require('../errors')
 
 const getAllJobs = async (req,res)=> {
-res.send('get all jobs')
+  //use find for filtering
+const jobs = await Jobs.find({createdBy:req.user.userId}).sort('createdAt')
+res.status(StatusCodes.OK).json({jobs, count:jobs.length})
 }
 
 const getJob = async (req,res)=> {
-  res.send('get a single job')
+  const {user:{userId},params:{id:jobId}} = req
+  const job = await Jobs.findOne({
+    _id:jobId, createdBy:userId
+  })
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
  }
 
 const createJobs = async (req,res)=> {
@@ -16,11 +25,39 @@ const createJobs = async (req,res)=> {
   const job = await Jobs.create(req.body)
    res.status(StatusCodes.CREATED).json({ job })
  }
+
 const updateJobs = async (req,res)=> {
-  res.send('update  jobs')
+  const {
+    body:{company,position},
+     user: {userId},
+      params:{id:jobId}
+    } = req
+if (company === "" || position === "") {
+  throw new BadRequestError('Company or position fields cannot be empty')
+}
+const job = await Jobs.findByIdAndUpdate({_id:jobId, createdBy:userId},req.body, {new:true,
+  runValidators: true}
+  )
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
+
   }
 const deleteJob = async (req,res)=> {
-  res.send('delete a job')
+  const {
+      user: {userId},
+      params:{id:jobId}
+    } = req
+  const job = await Jobs.findByIdAndDelete({
+    _id:jobId,
+    createdBy:userId
+  })
+  if(!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
+
   }        
   module.exports = {
     getAllJobs,
