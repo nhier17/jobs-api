1d76b068a309722447327646e084679f8334ffeb
Abraham <abrahamnhier97@gmail.com>
2024-01-25 10:51:55 +0300

    restructuring the code

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
 const userSchema = new mongoose.Schema({
    name: { 
     type: String,
@@ -21,5 +23,12 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
 
    },
+
 })
+userSchema.pre('save', async function() {
+   const salt = await bcrypt.genSalt(10);
+   this.password =  await bcrypt.hash(this.password,salt)
+   
+})
+
 module.exports = mongoose.model('User', userSchema)
