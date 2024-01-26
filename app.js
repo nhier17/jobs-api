f2d60d11dcd89cac9ee372b6e23520d4be3863fc
Abraham <abrahamnhier97@gmail.com>
2024-01-26 10:52:05 +0300

    finished extra security set up

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const connectDB = require('./db/connect')
 const authentificateUSer = require('./middleware/authentification')
 
@@ -13,9 +19,18 @@ const notFound = require('./middleware/not-found')
 
 const authRouter = require('./Routes/auth')
 const jobsRouter = require('./Routes/jobs')
-
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each ip to 100 requests per window
+}))
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authentificateUSer, jobsRouter)
